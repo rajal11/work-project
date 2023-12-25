359a584cb3c3e02dc8055e307b58e4506191d7c8
rajal11 <146832706+rajal11@users.noreply.github.com>
2023-12-25 22:43:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let facebook = document.getElementById('fblogo');
+console.log(facebook)
+
+facebook.addEventListener('click',function(){
+    // location.href='https://www.facebook.com/';
+    window.open('https://www.facebook.com/','_blank')
+})
